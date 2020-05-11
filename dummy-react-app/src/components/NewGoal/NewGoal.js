059f896ee2eb27b8e82599e0e1af964f31e86fcf
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-05-11 20:13:13 +0300

    + new goal form state

--- dummy-react-app/src/components/NewGoal/NewGoal.js
+++ dummy-react-app/src/components/NewGoal/NewGoal.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewGoal.css';
 
 const NewGoal = props => {
+    let [goalInput, setGoalInput] = useState('');
+
+    const inputChangeHandler = evt => {
+        setGoalInput(evt.target.value);
+    }
+
     const addGoalHandler = evt => {
         evt.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'Dummy Text'
+            text: goalInput
         }
+
+        setGoalInput('');
 
         props.onAddGoal(newGoal);
     };
 
     return (
         <form className="new-goal" onSubmit={addGoalHandler}>
-            <input type="text" />
+            <input type="text" value={goalInput} onChange={inputChangeHandler} />
             <button type="submit">Add goal</button>
         </form>
     );
