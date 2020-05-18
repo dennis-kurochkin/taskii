010aab0eef295668cde6a4374042a25b13ae17ac
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-05-18 18:26:54 +0300

    + custom form input

--- learning/frontend/src/places/pages/NewPlace.js
+++ learning/frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import './NewPlace.css';
+import Input from '../../shared/components/FormElements/Input';
+
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  )
 };
 
 export default NewPlace;
